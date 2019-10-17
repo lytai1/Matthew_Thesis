f1597b860ad6984f0a56cdb3417c9eed9aedb4c0
Matthew Jones <boywithstickx@yahoo.com>
2019-10-16 23:59:31 -0700

    Worked on odfs generation

--- Code/DTI_2_COMP/diffusion_imaging/preprocessing/preprocessing.py
+++ Code/DTI_2_COMP/diffusion_imaging/preprocessing/preprocessing.py
@@ -26,11 +26,11 @@ class Mask(Preprocess):
         maskdata, mask = median_otsu(image, vol_idx=[0, 1], median_radius=4, numpass=2,
                                      autocrop=False, dilate=1)
 
-        axial_slice = 40
-        mask_roi = np.zeros(image.shape[:-1], dtype=bool)
-        mask_roi[:, :, axial_slice] = mask[:, :, axial_slice]
+        #axial_slice = 40
+        #mask_roi = np.zeros(image.shape[:-1], dtype=bool)
+        #mask_roi[:, :, axial_slice] = mask[:, :, axial_slice]
         
-        return mask_roi
+        return (maskdata, mask) 
     
     def process(self, mri_data):
         
