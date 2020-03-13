9ceaf8c2ff763e5ca96b94745d4169256e1a42fb
Matthew Jones <mjones@spartan01.cluster>
2020-03-13 00:31:39 -0700

    Added the white matter segmentation to the pipeline. Added the JHU-ICBM tract for the cingulum hippocampal region mask step. Added white matter segmentation using the T1

--- Code/NODDI/run_analysis.sh
+++ Code/NODDI/run_analysis.sh
@@ -21,6 +21,7 @@ p) PATIENT_NUM=${OPTARG};;
 esac
 done
 
+ORIGINAL_DIR=$PWD
 T1_DIR=$(dirname "${T1_PATH}")
 DTI_DIR=$(dirname "${DTI_PATH}")
 
@@ -35,6 +36,7 @@ REG_FILE_EXT="_reg${NII_FILE_EXT}"
 T1_OR_PATH="${T1_DIR}/${PATIENT_NUM}_T1${REORIENT_FILE_EXT}"
 T1_REF_PATH="${T1_DIR}/${PATIENT_NUM}_T1${REF_FILE_EXT}"
 T1_MAT_PATH="${T1_DIR}/${PATIENT_NUM}${MAT_FILE_EXT}"
+T1_SEG_PATH="${T1_DIR}/${PATIENT_NUM}${SEG_FILE_EXT}"
 
 DTI_OR_PATH="${DTI_DIR}/${PATIENT_NUM}_DTI${REORIENT_FILE_EXT}"
 DTI_SEG_PATH="${DTI_DIR}/${PATIENT_NUM}_DTI${SEG_FILE_EXT}"
@@ -43,6 +45,10 @@ DTI_CORR_PATH="${DTI_DIR}/${PATIENT_NUM}_DTI${CORRECTION_FILE_EXT}"
 DTI_REG_PATH="${DTI_DIR}/${PATIENT_NUM}_DTI${REG_FILE_EXT}"
 
 RESULTS_DIR="${DTI_DIR}/${PATIENT_NUM}" 
+WHITE_MATTER_SEG_PATH="${DTI_DIR}/WHITE_MATTER_SEGMENTATION" 
+TRACTS_PATH="~/tracts"
+LEFT_CINGULUM_HIPPO_PATH="${TRACTS_PATH}/left_cingulum_hippo.nii.gz"
+RIGHT_CINGULUM_HIPPO_PATH="${TRACTS_PATH}/right_cingulum_hippo.nii.gz"
 
 sbatch <<EOT
 #!/bin/bash
@@ -54,6 +60,7 @@ set -ex
 ## This step makes certain that the patients data is in the correct orientation
 echo "Orienting DTI to standard space"
 fslreorient2std $DTI_PATH $DTI_OR_PATH
+fslreorient2std $T1_PATH $T1_OR_PATH
 
 ## This step runs the segmentation protocol
 echo "Running BET to remove extra material with options -R -B -F"
@@ -61,6 +68,11 @@ echo "-R being the option for robust (run multiple iterations)"
 echo "-B being extract eyes"
 echo "-F being run on all volumes (4D file)"
 bet $DTI_OR_PATH $DTI_SEG_PATH -R -B -F
+bet $T1_OR_PATH $T1_SEG_PATH -R -B -F
+
+## Segment the T1 brain into 3 categories for use in the resulting noddi image
+mkdir -p "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION"
+fast -n 3 -o "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION/${PATIENT_NUM}" ${T1_SEG_PATH}
 
 ## This step runs the movement correction on the patient data
 echo "Running Eddy correction to compensate for patient movement"
@@ -89,4 +101,24 @@ cp "${DTI_DIR}/${PATIENT_NUM}_brain_mask${NII_FILE_EXT}" "${RESULTS_DIR}/mask${N
 
 echo "Running NODDI analysis"
 python run_noddi.py --path $RESULTS_DIR --model 1 --label adni --retrain
+
+## Segment the white matter via the T1
+fslmaths "${RESULTS_DIR}/odi.nii.gz" -mas "${WHITE_MATTER_SEG_PATH}/${PATIENT_NUM}_pve_2.nii.gz" "${RESULTS_DIR}/odi_segmented.nii.gz"
+
+## Check to see if the tract has been generated
+if [ ! -f "${LEFT_CINGULUM_HIPPO_PATH} ]; then
+	# split the JHU-ICBM-tracts file
+	cd $TRACTS_PATH
+	fslsplit /usr/local/fsl/data/atlases/JHU/JHU-ICBM-tracts-prob-2mm.nii.gz
+	# rename the 6th volume to left_cingulum_hippo.nii.gz
+	# rename the 7th volume to right_cingulum_hippo.nii.gz
+	mv "${TRACTS_PATH}/vol0006.nii.gz" "${LEFT_CINGULUM_HIPPO_PATH}"
+	mv "${TRACTS_PATH}/vol0007.nii.gz" "${RIGHT_CINGULUM_HIPPO_PATH}"
+	cd $ORIGINAL_DIR	
+fi
+## Segment the left cingulum hippocampal region
+fslmaths "${RESULTS_DIR}/odi_segmented.nii.gz" -mas "${LEFT_CINGULUM_HIPPO_PATH}" "${RESULTS_DIR}/odi_left_cingulum_hippo.nii.gz"
+
+## Segment the right cingulum hiipocampal region
+fslmaths "${RESULTS_DIR}/odi_segmented.nii.gz" -mas "${RIGHT_CINGULUM_HIPPO_PATH}" "${RESULTS_DIR}/odi_right_cingulum_hippo.nii.gz"
 EOT
