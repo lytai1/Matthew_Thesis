24a4b93f3b5ef30dc9cf7f0652fa9546b66c8f77
Matthew Jones <mjones@spartan01.cluster>
2020-03-15 23:47:50 -0700

    Completely automated the end to end pipeline

--- Code/NODDI/run_analysis.sh
+++ Code/NODDI/run_analysis.sh
@@ -10,8 +10,9 @@ Sample run script:
 sbatch run_analysis.sh -t some/path/to/patient#_T1.nii.gz -d some/path/to/patient#.nii.gz -r some/path/to/avg152.nii.gz -p ###_S_####
 DOCS
 
-while getopts 'tdrp' flag; do
-   case "${flag}" in
+while getopts t:d:r:p: option
+   do
+   case "${option}" in
       t) T1_PATH="${OPTARG}" ;;
       d) DTI_PATH=${OPTARG};;
       r) REF_PATH=${OPTARG};;
@@ -56,8 +57,8 @@ DTI_REG_PATH="${DTI_DIR}/${PATIENT_NUM}_DTI${REG_FILE_EXT}"
 
 RESULTS_DIR="${DTI_DIR}/${PATIENT_NUM}"
 
-WHITE_MATTER_SEG_PATH="${DTI_DIR}/WHITE_MATTER_SEGMENTATION" 
-TRACTS_PATH="~/tracts"
+WHITE_MATTER_SEG_PATH="${DTI_DIR}/${PATIENT_NUM}/WHITE_MATTER_SEGMENTATION" 
+TRACTS_PATH="${data_dir}/tracts"
 LEFT_CINGULUM_HIPPO_PATH="${TRACTS_PATH}/left_cingulum_hippo.nii.gz"
 RIGHT_CINGULUM_HIPPO_PATH="${TRACTS_PATH}/right_cingulum_hippo.nii.gz"
 
@@ -67,11 +68,18 @@ sbatch <<EOT
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=20GB
 
-set -ex
-## This step makes certain that the patients data is in the correct orientation
+set -e
+## These two steps make certain that the patients data is in the correct orientation
 echo "Orienting DTI to standard space"
-[[ ! -f $DTI_OR_PATH ]] && fslreorient2std $DTI_PATH $DTI_OR_PATH
-[[ ! -f $T1_OR_PATH ]] && fslreorient2std $T1_PATH $T1_OR_PATH
+if [[ ! -f $DTI_OR_PATH ]]; then
+  echo "Orienting DTI to standard space"
+  fslreorient2std $DTI_PATH $DTI_OR_PATH
+fi
+
+if [[ ! -f $T1_OR_PATH ]]; then
+  echo "Orienting T1 to standard space" 
+  fslreorient2std $T1_PATH $T1_OR_PATH
+fi
 
 ## This step runs the segmentation protocol
 echo "Running BET to remove extra material with options -R -B -F"
@@ -81,35 +89,35 @@ echo "-F being run on all volumes (4D file)"
 [[ ! -f $DTI_SEG_PATH ]] && bet $DTI_OR_PATH $DTI_SEG_PATH -R -B -F
 [[ ! -f $T1_SEG_PATH ]] && bet $T1_OR_PATH $T1_SEG_PATH -R -B -F
 
-## Segment the T1 brain into 3 categories for use in the resulting noddi image
-if [[ ! -f "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION/${PATIENT_NUM}_pve_2.nii.gz" ]]; then
-  mkdir -p "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION"
-  fast -n 3 -o "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION/${PATIENT_NUM}" ${T1_SEG_PATH}
-fi
-
 ## This step runs the movement correction on the patient data
 if [[ ! -f "${DTI_CORR_PATH}" ]]; then
   echo "Running Eddy correction to compensate for patient movement"
   eddy_correct $DTI_SEG_PATH $DTI_CORR_PATH trilinear
 fi
 
-#echo "Running flirt on the T1 with a reference to the 2mm MNI reference"
-if [[ ! -f "${T1_REG_PATH}" ]]; then
-  flirt -in $T1_OR_PATH -ref $REF_PATH -omat "${T1_DIR}/${PATIENT_NUM}_T1.mat"
-  flirt -in $T1_OR_PATH -ref $REF_PATH -out $T1_REG_PATH -applyxfm -init "${DTI_DIR}/${PATIENT_NUM}_T1.mat"
+if [[ ! -f "${T1_SEG_PATH}" ]]; then
+  echo "Running flirt on the T1 with a reference to the 2mm MNI reference"
+  flirt -in $T1_SEG_PATH -ref $REF_PATH -omat "${T1_DIR}/${PATIENT_NUM}_T1.mat"
+  flirt -in $T1_SEG_PATH -ref $REF_PATH -out $T1_REG_PATH -applyxfm -init "${DTI_DIR}/${PATIENT_NUM}_T1.mat"
 fi
 
 ## This is the step that registers the patients scans into a known template space for use in extracting specific regions later
-echo "Running flirt on the DTI with a reference to the 2mm MNI template."
 if [[ ! -f "${DTI_REG_PATH}" ]]; then
+  echo "Running flirt on the DTI with a reference to the 2mm MNI template."
   flirt -in $DTI_CORR_PATH -ref $REF_PATH -omat "${DTI_DIR}/${PATIENT_NUM}.mat"
   flirt -in $DTI_CORR_PATH -ref $REF_PATH -out $DTI_REG_PATH -applyxfm -init "${DTI_DIR}/${PATIENT_NUM}.mat"
 fi
 
 ## generating binary mask of registered data
-echo "Generating binary mask from registered data"
 if [[ ! -f "${DTI_DIR}/${PATIENT_NUM}_brain${NII_FILE_EXT}" ]]; then
+  echo "Generating binary mask from registered data"
   bet $DTI_REG_PATH "${DTI_DIR}/${PATIENT_NUM}_brain${NII_FILE_EXT}" -m -F
+fi
+
+## Segment the T1 brain into 3 categories for use in the resulting noddi image
+if [[ ! -f "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION/${PATIENT_NUM}_pve_2.nii.gz" ]]; then
+  mkdir -p "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION"
+  fast -n 3 -o "${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION/${PATIENT_NUM}" ${T1_REG_PATH}
 fi
 
 echo "The resulting file can be found here: ${DTI_REG_PATH}"
@@ -125,13 +133,16 @@ echo "Running NODDI analysis"
 python run_noddi.py --path $RESULTS_DIR --model 1 --label adni 
 
 ## Segment the white matter via the T1
+echo "Masking the odi values generated with the white matter segmentation from the patients T1"
 fslmaths "${RESULTS_DIR}/odi.nii.gz" -mas "${WHITE_MATTER_SEG_PATH}/${PATIENT_NUM}_pve_2.nii.gz" "${RESULTS_DIR}/odi_segmented.nii.gz"
 
 ## Check to see if the tract has been generated
-if [ ! -f "${LEFT_CINGULUM_HIPPO_PATH} ]; then
+if [[ ! -f "${LEFT_CINGULUM_HIPPO_PATH}" ]]; then
+        echo "No tract volumes were found. Preparing the JHU-ICBM tract segmentations"
 	# split the JHU-ICBM-tracts file
+        mkdir -p $TRACTS_PATH
 	cd $TRACTS_PATH
-	fslsplit /usr/local/fsl/data/atlases/JHU/JHU-ICBM-tracts-prob-2mm.nii.gz
+	fslsplit "${FSLDIR}/data/atlases/JHU/JHU-ICBM-tracts-prob-2mm.nii.gz"
 	# rename the 6th volume to left_cingulum_hippo.nii.gz
 	# rename the 7th volume to right_cingulum_hippo.nii.gz
 	mv "${TRACTS_PATH}/vol0006.nii.gz" "${LEFT_CINGULUM_HIPPO_PATH}"
@@ -139,8 +150,10 @@ if [ ! -f "${LEFT_CINGULUM_HIPPO_PATH} ]; then
 	cd $ORIGINAL_DIR	
 fi
 # Segment the left cingulum hippocampal region
+echo "Segmenting the left cingulum hippocampal region using the white matter segmented odi values"
 fslmaths "${RESULTS_DIR}/odi_segmented.nii.gz" -mas "${LEFT_CINGULUM_HIPPO_PATH}" "${RESULTS_DIR}/odi_left_cingulum_hippo.nii.gz"
 
 ## Segment the right cingulum hiipocampal region
+echo "Segmenting the right cingulum hippocampal region using the white matter segmented odi values"
 fslmaths "${RESULTS_DIR}/odi_segmented.nii.gz" -mas "${RIGHT_CINGULUM_HIPPO_PATH}" "${RESULTS_DIR}/odi_right_cingulum_hippo.nii.gz"
 EOT
