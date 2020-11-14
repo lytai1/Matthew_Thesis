7e791abef5e697d429178258e5b3663ff5faa569
Lok Yi Tai <lokyi.tai@sjsu.edu>
2020-11-13 21:29:55 -0800

    make insert stats work with info csv or not

--- Code/NODDI/insert_stats.py
+++ Code/NODDI/insert_stats.py
@@ -74,9 +74,9 @@ def insert_stats(stats: dict, viscode: str, ptid: str, dataframe: pd.DataFrame) 
         idx = dataframe.loc[mask].index.values[0]
         dataframe.at[idx, key] = value
         '''
-        dataframe.loc[(ptid, viscode), str(key)] = value
+        dataframe.loc[(ptid, viscode), key] = value
             
-        print(dataframe.loc[mask, key])
+        print(dataframe.loc[(ptid, viscode), key])
 
     return dataframe
 
