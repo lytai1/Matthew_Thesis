317f1dba101e30aa064cdfd0576042cf773aa5cf
Lok Yi Tai <lokyi.tai@sjsu.edu>
2020-12-30 16:02:05 -0800

    added command line in run_analysis_multiple.sh

--- Code/NODDI/run_analysis_multiple.sh
+++ Code/NODDI/run_analysis_multiple.sh
@@ -14,8 +14,10 @@ IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read s_id viscode
 do
+	echo "run noddi of"
 	echo "s_id : $s_id"
 	echo "viscode : $viscode"
+	PATIENT_ID=$s_id VISCODE=$viscode; bash run_analysis.sh -t $adni_dir/$PATIENT_ID/$VISCODE/"${PATIENT_ID}_${VISCODE}"_T1.nii -d $adni_dir/$PATIENT_ID/$VISCODE/"${PATIENT_ID}_${VISCODE}".nii -r $mni_dir/avg152T1_brain.nii -p "${PATIENT_ID}"_$VISCODE
 
 done < $INPUT
 IFS=$OLDIFS
