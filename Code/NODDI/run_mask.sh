ee2773077917a31f667f5c0acc73266753192615
Lok Yi Tai <lokyi.tai@sjsu.edu>
2021-01-03 11:54:38 -0800

    debugged run_mask.sh

--- Code/NODDI/run_mask.sh
+++ Code/NODDI/run_mask.sh
@@ -10,7 +10,7 @@ options:
    -r The reference standard template to register against
    -p The patient number to use as the name
 
-bash run_mask.sh -d /home/ltai/mci_di/data_test/ADNI -p 003_S_6264 -v bl -m anterior_thalamic_radiation_l -n 0000 -f /home/ltai/fsl
+bash run_mask.sh -d /home/ltai/mci_di/andi3_data/test/ADNI -p 003_S_6264 -v bl -m anterior_thalamic_radiation_l -n 0000 -f /home/ltai/fsl
 DOCS
 
 while getopts d:p:v:m:n:f: option
@@ -29,7 +29,7 @@ done
 ORIGINAL_DIR=$PWD
 
 PATIENT_DIR="${ADNI_DIR}/${PATIENT_NUM}/${VISCODE}"
-RESULTS_DIR="${PATIENT_DIR}/${PATIENT_NUM}"
+RESULTS_DIR="${PATIENT_DIR}/${PATIENT_NUM}_${VISCODE}"
 WHITE_MATTER_SEG_PATH="${RESULTS_DIR}/WHITE_MATTER_SEGMENTATION" 
 
 TRACTS_PATH="$(dirname "${ADNI_DIR}")/tracts"
@@ -40,6 +40,7 @@ MASK_PATH="${TRACTS_PATH}/${MASK}.nii.gz"
 ## ignore everything that is not white matter
 echo "Masking the odi values generated with the white matter segmentation from the patients T1"
 fslmaths "${RESULTS_DIR}/odi.nii.gz" -mas "${WHITE_MATTER_SEG_PATH}/${PATIENT_NUM}_pve_2.nii.gz" "${RESULTS_DIR}/odi_segmented.nii.gz"
+
 
 ## Check to see if the tract has been generated
 ## split the tracts in one file to individual ones
