340b1576c8240c246c08dfe9e0541d079b7968ea
Lok Yi Tai <lokyi.tai@sjsu.edu>
2021-01-18 13:54:17 -0800

    debugged insert_stats_multiple.py

--- Code/NODDI/insert_stats_multiple.py
+++ Code/NODDI/insert_stats_multiple.py
@@ -88,7 +88,7 @@ class InsertStats:
         return dataframe
 
     # Main function
-    def post_process_run(self, path, adni_merge_path=None, label=None):
+    def post_process_run(self, path, label=None):
         """This function acts as the main function for the stat generation part of this program.
 
         Args:
@@ -120,7 +120,7 @@ class InsertStats:
             
             for m_row in self.mask_df.itertuples():
                 adni_merge_path = os.path.join(path, p_row.PTID + "_" + p_row.VISCODE + "_odi_" + m_row.name + ".nii.gz")
-                self.post_process_run(path, adni_merge_path, m_row.name)
+                self.post_process_run(adni_merge_path, m_row.name)
             
 
 if __name__ == "__main__":
