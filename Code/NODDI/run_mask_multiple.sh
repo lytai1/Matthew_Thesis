a5dfad5304be2aff13c8b9252cb435e7bb06bba0
Lok Yi Tai <lokyi.tai@sjsu.edu>
2021-01-22 13:20:49 -0800

    cleaned up run_mask_multiple.sh

--- Code/NODDI/run_mask_multiple.sh
+++ Code/NODDI/run_mask_multiple.sh
@@ -4,8 +4,8 @@
 # ------------------------------------------
 
 
-ADNI_DIR="/home/ltai/mci_di/andi3_data/ad/ADNI"
-INPUT_PATIENT_LIST="/home/ltai/mci_di/andi3_data/ad/adni3_ad_list.csv"
+ADNI_DIR="/home/ltai/mci_di/andi3_data/test/ADNI"
+INPUT_PATIENT_LIST="/home/ltai/mci_di/andi3_data/test/adni3_test_list.csv"
 INPUT_JHU_LIST="/home/ltai/mci_di/andi3_data/jhu_mask_list.csv"
 FSL_DIR="/home/ltai/fsl"
 
@@ -13,13 +13,12 @@ ORIGINAL_DIR=$PWD
 TRACTS_PATH="$(dirname "${ADNI_DIR}")/tracts"
 
 
-
 ## Check to see if the tract has been generated
 ## split the tracts in one file to individual ones
 if [[ ! -f "${TRACTS_PATH}" ]]; then 
 	echo "No tract volumes were found. Preparing the JHU-ICBM tract segmentations"
 	# split the JHU-ICBM-tracts file
-        mkdir -p $TRACTS_PATH
+	mkdir -p $TRACTS_PATH
 	cd $TRACTS_PATH
 	fslsplit "${FSL_DIR}/data/atlases/JHU/JHU-ICBM-tracts-prob-2mm.nii.gz"
 	cd $ORIGINAL_DIR	
@@ -59,10 +58,8 @@ do
 	[ ! -f $INPUT_JHU_LIST ] && { echo "$INPUT_JHU_LIST file not found"; exit 99; }
 	while read m_id mask_name
 	do
-		echo "Check if odi value of $mask_name exists."
 		if [[ ! -f "${RESULTS_DIR}/${patient_no}_${viscode}_odi_${mask_name}.nii.gz" ]]; then 
 			MASK_PATH="${TRACTS_PATH}/${mask_name}.nii.gz"
-			echo "odi of $mask_name for this patient does not exist"
 			echo "Segmenting the ${mask_name} region using the white matter segmented odi values"
 			fslmaths "${RESULTS_DIR}/odi_segmented.nii.gz" -mas "${MASK_PATH}" "${RESULTS_DIR}/${patient_no}_${viscode}_odi_${mask_name}.nii.gz"
 		fi
