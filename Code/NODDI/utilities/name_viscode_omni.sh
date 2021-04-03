f431ea09e111e31d71e6ac3488acbaaffd56b384
Kathleen Tai <lytai1@Kathleens-MacBook-Air.local>
2021-04-03 11:13:31 -0700

    debugged name viscode

--- Code/NODDI/utilities/name_viscode_omni.sh
+++ Code/NODDI/utilities/name_viscode_omni.sh
@@ -1,4 +1,12 @@
-for patient_no in *
+while getopts d: option
+	do
+	case "${option}" in
+		d) ADNI_DIR=${OPTARG};;
+		*) INVALID_ARGS=${OPTARG};;
+	esac
+done
+
+for patient_no in ${ANDI_DIR}/*
 do 
     echo patient_no/*
 done
